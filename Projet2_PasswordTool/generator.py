385ca95e66958208b06bef9e81817e3ce8ef8ae0
emma <emma@kali>
2026-04-19 21:31:08 +0100

    Modification de generator.py : Ajouts des typages

--- Projet2_PasswordTool/generator.py
+++ Projet2_PasswordTool/generator.py
@@ -1,18 +1,25 @@
 # generator.py
 import secrets
 import string
+from typing import Optional
 
-def generer_mot_de_passe(longueur=12, maj=True, chiffres=True, speciaux=True):
-    # Base : lettres minuscules (toujours incluses pour la diversité)
-    caracteres = string.ascii_lowercase
+def generer_mot_de_passe(
+    longueur: int = 12, 
+    inclure_maj: bool = True, 
+    inclure_chiffres: bool = True, 
+    inclure_speciaux: bool = True
+) -> str:
     
-    if maj:
-        caracteres += string.ascii_uppercase
-    if chiffres:
-        caracteres += string.digits
-    if speciaux:
-        caracteres += string.punctuation
+    # Alphabet de base (minuscules)
+    alphabet = string.ascii_lowercase
+    
+    if inclure_maj:
+        alphabet += string.ascii_uppercase
+    if inclure_chiffres:
+        alphabet += string.digits
+    if inclure_speciaux:
+        alphabet += string.punctuation
         
-    # Génération sécurisée [cite: 34, 61]
-    mdp = ''.join(secrets.choice(caracteres) for _ in range(longueur))
+    # secrets.choice est utilisé pour la sécurité au lieu de random.choice
+    mdp = ''.join(secrets.choice(alphabet) for _ in range(longueur))
     return mdp
